d602c06b6e1314f2ac7bdeec4fbdead133ff1c45
Himal Subedi <itissubedi@gmail.com>
2025-05-15 07:59:52 +0545

    Inital Commit

---
+++ class1/practice.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+
+public class practice{
+    public static void main(String[] args){
+        BigInteger a = new BigInteger("123981233222123123123");
+        BigInteger b = new BigInteger("123981233222123123123");
+        BigInteger sum = a.add(b);
+        System.out.println("\n sum :" + sum);
+
+        System.out.println(a  +"\n");
+        // Big Int Stores More almost infinite than long
+        // long b = 123981233222123123123L;
+        // System.out.println(b);
+
+        boolean c = false;
+        System.out.println(c);
+    }
+}
