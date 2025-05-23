c042548d278a5df6f63a6fc69388c93d6a551cda
Himal Subedi <itissubedi@gmail.com>
2025-05-23 07:26:08 +0545

    Updated class1

--- class1/datatypes.java
+++ class1/datatypes.java
@@ -1,6 +1,6 @@
 import java.math.BigInteger;
 
-public class practice{
+public class datatypes{
     public static void main(String[] args){
         BigInteger a = new BigInteger("123981233222123123123");
         BigInteger b = new BigInteger("123981233222123123123");
@@ -11,6 +11,8 @@ public class practice{
         // Big Int Stores More almost infinite than long
         // long b = 123981233222123123123L;
         // System.out.println(b);
+
+        //float 4 bytes incloduing point, double can store upto 8 bytes
 
         boolean c = false;
         System.out.println(c);
